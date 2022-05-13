136600ab778e7edb5f0486b0fb9c25b56bbd4b5f
Ramil Nazmeev <ramil@nazmeev.ru>
2022-05-13 17:09:23 +0400

    updated app

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,77 +5,16 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import altair as alt
 
+from utils import load_data
+
 with st.echo(code_location='below'):
-    """
-    ## Hello, World!
-    """
+    st.title("Titanic data exploration")
 
+    data_load_state = st.text('Loading data...')
 
-    def print_hello(name="World"):
-        st.write(f"### Hello, {name}!")
+    df = load_data()
 
+    data_load_state.text("Done! (using st.cache)")
 
-    name = st.text_input("Your name", key="name", value="Anonymous")
-    print_hello(name)
-
-    """
-    ## Добавим графики
-    Чтобы заработали библиотеки seaborn и altair, нужно добавить в проект файл 
-    `requirements.txt` с такими строчками:
-    
-        seaborn
-        altair
-    """
-
-
-    a = st.slider("a")
-    x = np.linspace(-6, 6, 500)
-    df = pd.DataFrame(dict(x=x, y=np.sin(a * x)))
-    fig, ax = plt.subplots()
-    sns.lineplot(data=df, x="x", y="y", ax=ax)
-    st.pyplot(fig)
-
-    """
-    ## Немного анализа данных
-    """
-
-
-    @st.cache
-    def get_data():
-        data_url = (
-            "https://github.com/Godoy/imdb-5000-movie-dataset/raw/"
-            "master/data/movie_metadata.csv"
-        )
-        return (
-            pd.read_csv(data_url)
-            .dropna(subset=["title_year"])
-            .assign(
-                title_year=lambda x: pd.to_datetime(
-                    x["title_year"], format="%Y"
-                )
-            )
-        )
-
-
-    df = get_data()
-
-    director = st.selectbox(
-        "Director", df["director_name"].value_counts().iloc[:10].index
-    )
-
-    df_selection = df[lambda x: x["director_name"] == director]
-    df_selection
-
-    chart = (
-        alt.Chart(df_selection)
-        .mark_circle()
-        .encode(x=alt.X("title_year:T"), y="imdb_score", tooltip="movie_title")
-    )
-
-    st.altair_chart(
-        (
-            chart
-            + chart.transform_loess("title_year", "imdb_score").mark_line()
-        ).interactive()
-        # .transform_loess добавляет сглаживающую кривую
-    )
+    st.subheader('Raw Titanic data')
+    st.write(df)
