2bd9bc99262c6ab538851cf301d8522b5ecf40df
Ramil Nazmeev <ramil@nazmeev.ru>
2022-05-13 21:02:08 +0400

    added port of embarkation

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,18 @@ with st.echo(code_location='below'):
 
     # Heatmap of survivals between classes
     st.subheader('Heatmap of survivals between classes')
+    plt.title(
+        "Heatmap of survivals between classes", fontsize=13)
     fig = plt.figure(figsize=(16, 9))
     group = df.groupby(['Pclass', 'Survived'])
     pclass_survived = group.size().unstack()
     sns.heatmap(pclass_survived, annot=True, fmt="d")
+    st.pyplot(fig)
+
+    # Port of Embarkation
+    st.subheader('Distributions of survivals dependent on port of Embarkation')
+    fig = plt.figure(figsize=(16, 9))
+    plt.title("Distributions of survivals dependent on port of Embarkation", fontsize=13)
+    sns.catplot(x='Embarked', hue='Survived',
+                kind='count', col='Pclass', data=df)
     st.pyplot(fig)
